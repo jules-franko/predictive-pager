f471f10024c8a4864869caf751562cb5b53d42c3
jules-franko <julesfranko@proton.me>
2025-11-12 19:43:46 -0700

    m

--- pager-predict.c
+++ pager-predict.c
@@ -23,7 +23,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
 
     if (!initialized) {
 		for (int proc=0; proc < MAXPROCESSES; proc++) {
-			for (page=0; page < MAXPROCPAGES; page++) {
+			for (int page=0; page < MAXPROCPAGES; page++) {
 				timestamps[proc][page] = 0;
 			}
 		}
