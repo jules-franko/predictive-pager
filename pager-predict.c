f5a019e08130c356acc5d8bf461df12f7383bfab
jules-franko <julesfranko@proton.me>
2025-11-13 21:46:20 -0700

    m

--- pager-predict.c
+++ pager-predict.c
@@ -15,11 +15,10 @@ void pageit(Pentry q[MAXPROCESSES]) {
     /* Local vars */
     static int timestamps[MAXPROCESSES][MAXPROCPAGES];
 
-    /* initialize static vars on first run */
- //    if(!initialized){
-	// /* Init complex static vars here */
-	// initialized = 1;
- //    }
+	int pc;
+	int page;
+	int lru_page;
+	int oldest_tick;
 
     if (!initialized) {
 		for (int proc=0; proc < MAXPROCESSES; proc++) {
@@ -29,56 +28,96 @@ void pageit(Pentry q[MAXPROCESSES]) {
 		}
 		initialized = 1;
 	}
-    
-    /* TODO: Implement Predictive Paging */
-    //fprintf(stderr, "pager-predict not yet implemented. Exiting...\n");
-    //exit(EXIT_FAILURE);
 
 	for (int proc = 0; proc < MAXPROCESSES; proc++) {
 
-		if (!q[proc].active) continue;
-
-		int pc = q[proc].pc;
-		int page = pc/PAGESIZE;
-
-		timestamps[proc][page] = tick;
-
-		if (q[proc].pages[page]) { continue; }
-		if (pagein(proc, page)) {
+		if (q[proc].active) {
+			/*Look for a predicted page to page in*/
+			/*pagein()*/
+			/*If it fails, use LRU to evict a page on page fault*/
+		} else {
 			continue;
 		}
 
-		int lru_proc = 0;
-		int lru_page = 0;
-		int oldest_time = tick;
-		for (int j = 0; j < MAXPROCESSES; j++) {
+		pc = q[proc].pc;
+		page = pc/PAGESIZE;
 
-			if (!q[j].active) { continue; }
+		/*Handle current page if it's not swapped in*/
+		if (!q[proc].pages[page]) {
 
-			for (int i = 0; i < MAXPROCPAGES; i++) {
-				if (j == proc && i == page) { continue; }
-				if (q[j].pages[i] == 0) { continue; }
+			/*Try to page it in*/
+			if (!pagein(proc, page)) {
 
-				if (timestamps[j][i] < oldest_time) {
-					oldest_time = timestamps[j][i];
-					lru_proc = j;
-					lru_page = i;
+				lru_page = 0;
+				oldest_tick = tick; //Initialized to young time
+
+				/*If paging fails, use Local LRU to evict something*/
+				for (int i = 0; i < MAXPROCPAGES; i++) {
+
+					if (q[proc].pages[i] && i != page) {
+						if (timestamps[proc][i] < oldest_time) {
+							oldest_tick = timestamps[proc][i];
+							lru_page = i;
+						}
+					}
 				}
+
+				pageout(proc, lru_page);
 			}
+
+			}
+
+			timestamps[proc][page] = tick;
 		}
-
-		pageout(lru_proc, lru_page);
-
-		if (page+1 < MAXPROCPAGES && !q[proc].pages[page+1]) {
-			pagein(proc,page+1);
-		}
-
 	}
 
 
     /* advance time for next pageit iteration */
     tick++;
 } 
+
+// for (int proc = 0; proc < MAXPROCESSES; proc++) {
+//
+// 	if (!q[proc].active) continue;
+//
+// 	int pc = q[proc].pc;
+// 	int page = pc/PAGESIZE;
+//
+// 	timestamps[proc][page] = tick;
+//
+// 	int lru_proc = 0;
+// 	int lru_page = 0;
+// 	int oldest_tick = tick; //Initialized to young time
+//
+// 	int predicted_page;
+//
+// 	if (q[proc].pages[page]) { continue; }
+// 	if (pagein(proc, page)) { continue; }
+//
+// 	//If pagein() fails, choose something to evict
+// 	for (int process = 0; process < MAXPROCESSES; process++) {
+//
+// 		if (!q[process].active) { continue; }
+//
+// 		for (int i = 0; i < MAXPROCPAGES; i++) {
+// 			if (process == proc && i == page) { continue; }
+// 			if (q[process].pages[i] == 0) { continue; }
+//
+// 			if (timestamps[process][i] < oldest_time) {
+// 				oldest_tick = timestamps[process][i];
+// 				lru_proc = process;
+// 				lru_page = i;
+// 			}
+// 		}
+// 	}
+//
+// 	pageout(lru_proc, lru_page);
+//
+// 	// if (page+1 < MAXPROCPAGES && !q[proc].pages[page+1]) {
+// 	// 	pagein(proc,page+1);
+// 	// }
+//
+// }
 
 // for (int proc = 0; proc < MAXPROCESSES; proc++) {
 //
@@ -107,3 +146,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
 // 	pageout(proc, lru);
 //
 // }
+
+/* TODO: Implement Predictive Paging */
+//fprintf(stderr, "pager-predict not yet implemented. Exiting...\n");
+//exit(EXIT_FAILURE);
